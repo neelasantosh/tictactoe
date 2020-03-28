b355fc36005b94da0eef2c206c03a18d8b24e200
neelasantosh <neela.santosh2405@gmail.com>
2020-03-28 17:44:17 +0530

    Updates

---
+++ precache-manifest.cdae686e70987fa705b6ea92cedb790b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9a0f3c4e410d7953ae661b18ee11147",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "88874de27e817dfd3893",
+    "url": "/tictactoe/static/css/main.2afb2ac7.chunk.css"
+  },
+  {
+    "revision": "9a1a89c9b93171c3493b",
+    "url": "/tictactoe/static/js/2.ec9fda74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.ec9fda74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88874de27e817dfd3893",
+    "url": "/tictactoe/static/js/main.ea6555a5.chunk.js"
+  },
+  {
+    "revision": "d6a76b3547955ded53da",
+    "url": "/tictactoe/static/js/runtime-main.986fe205.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tictactoe/static/media/logo.5d5d9eef.svg"
+  }
+]);
